6614a1d6e981c92c2928f72a17cd4f65d25524b8
zhaohao <zhaoh@cecjiutian.com>
2026-05-08 15:15:32 +0800

    feat: init

---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+const {series, parallel, watch, src, dest} = require('gulp');
+const pump = require('pump');
+const fs = require('fs');
+const path = require('path');
+const order = require('ordered-read-streams');
+
+// gulp plugins and utils
+const livereload = require('gulp-livereload');
+const postcss = require('gulp-postcss');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const beeper = require('beeper');
+const zip = require('gulp-zip');
+
+// postcss plugins
+const easyimport = require('postcss-easy-import');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+// translations support
+const { mergeLocales } = require('@tryghost/theme-translations/build');
+const sharedThemeAssetsPath = path.dirname(require.resolve('@tryghost/shared-theme-assets/package.json'));
+
+function serve(done) {
+    livereload.listen();
+    done();
+}
+
+function handleError(done) {
+    return function (err) {
+        if (err) {
+            beeper();
+        }
+        return done(err);
+    };
+};
+
+function hbs(done) {
+    pump([
+        src(['*.hbs', 'partials/**/*.hbs']),
+        livereload()
+    ], handleError(done));
+}
+
+function css(done) {
+    pump([
+        src('assets/css/screen.css', {sourcemaps: true}),
+        postcss([
+            easyimport,
+            autoprefixer(),
+            cssnano()
+        ]),
+        dest('assets/built/', {sourcemaps: '.'}),
+        livereload()
+    ], handleError(done));
+}
+
+function getJsFiles(version) {
+    const jsFiles = [
+        src(`${sharedThemeAssetsPath}/assets/js/${version}/lib/**/*.js`),
+        src(`${sharedThemeAssetsPath}/assets/js/${version}/main.js`),
+    ];
+
+    if (fs.existsSync(`assets/js/lib`)) {
+        jsFiles.push(src(`assets/js/lib/*.js`));
+    }
+
+    jsFiles.push(src(`assets/js/main.js`));
+
+    return jsFiles;
+}
+
+function js(done) {
+    pump([
+        order(getJsFiles('v1'), {sourcemaps: true}),
+        concat('main.min.js'),
+        uglify(),
+        dest('assets/built/', {sourcemaps: '.'}),
+        livereload()
+    ], handleError(done));
+}
+
+function zipper(done) {
+    const filename = require('./package.json').name + '.zip';
+
+    pump([
+        src([
+            '**',
+            '!node_modules', '!node_modules/**',
+            '!dist', '!dist/**',
+            '!yarn-error.log'
+        ]),
+        zip(filename),
+        dest('dist/')
+    ], handleError(done));
+}
+
+function locales(done) {
+    mergeLocales({
+        local: './locales-local',
+        output: './locales'
+    })(done);
+}
+
+const localesWatcher = () => watch('./locales-local/**/*.json', locales);
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
+const cssWatcher = () => watch('assets/css/**/*.css', css);
+const jsWatcher = () => watch('assets/js/**/*.js', js);
+const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher, localesWatcher);
+const build = series(css, js, locales);
+
+exports.build = build;
+exports.zip = series(build, zipper);
+exports.default = series(build, serve, watcher);
